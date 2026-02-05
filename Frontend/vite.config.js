52c976d1417add05a801ad35c5d710dd902b3e92
Zayan <gazimohammadanondo@gmail.com>
2026-02-05 20:53:33 +0600

    .

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
-    minify: 'terser',
+   minify: "esbuild"
     chunkSizeWarningLimit: 1600,
   },
   server: {
